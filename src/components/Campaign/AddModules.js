63f8f61a628df85144accac1a404f9bdc793cf03
shlomi-atzmon <shlomiatzmon82@gmail.com>
2021-08-19 18:41:49 +0300

    set btn's footer on modules page

--- src/components/Campaign/AddModules.js
+++ src/components/Campaign/AddModules.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useHistory, Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 // DragDropContext = all of our colomns, Droppable colomn, Draggable card
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
@@ -6,7 +8,7 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { v4 as uuidv4 } from "uuid";
 
 const campaignModules = [
-  {id: uuidv4(), title: "Introdution", duration: "10", costPerRecipient: "0" },
+  { id: uuidv4(), title: "Introdution", duration: "10", costPerRecipient: "0" },
 ];
 
 const threatModules = [
@@ -95,93 +97,131 @@ const onDragEnd = (result, columns, setColumns) => {
 };
 
 const AddModules = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const onClick = () => {
+    // TODO - COUNT MODULES AND COST
+    dispatch({ type: "ADD_MODULES", payload: { modules: 5, cost: "5,000" } });
+    history.push("./schedule-campaign");
+  };
+
   const [columns, setColumns] = useState(columnsFromBackend);
   return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
-      <DragDropContext
-        onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+    <>
+      <div
+        style={{ display: "flex", justifyContent: "center", height: "100%" }}
       >
-        {Object.entries(columns).map(([columnId, column], index) => {
-          return (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-              key={columnId}
-            >
-              <h2>{column.name}</h2>
-              <div style={{ margin: 8 }}>
-                <Droppable droppableId={columnId} key={columnId}>
-                  {(provided, snapshot) => {
-                    return (
-                      <div
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        style={{
-                          padding: 4,
-                          width: 250,
-                          minHeight: 500,
-                        }}
-                      >
-                        {column.items.map((item, index) => {
-                          return (
-                            <Draggable
-                              key={item.id}
-                              draggableId={item.id}
-                              index={index}
-                            >
-                              {(provided, snapshot) => {
-                                return (
-                                  <div
-                                    ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    style={{
-                                      userSelect: "none",
-                                      padding: 16,
-                                      margin: "0 0 8px 0",
-                                      minHeight: "50px",
-                                      backgroundColor: snapshot.isDragging
-                                        ? "#263B4A"
-                                        : "#456C86",
-                                      color: "white",
-                                      ...provided.draggableProps.style,
-                                    }}
-                                  >
-                                    <div className="ui fluid card" key={index}>
-                                      <div className="content center aligned">
-                                        <div className="header">
-                                          {item.title}
-                                        </div>
-                                        <div className="description">
-                                          Playing Time{" "}
-                                          <b>{item.duration} Min</b>
-                                        </div>
-                                        <div className="description">
-                                          Cost Per Recipient{" "}
-                                          <b>{item.costPerRecipient}$</b>
+        <DragDropContext
+          onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+        >
+          {Object.entries(columns).map(([columnId, column], index) => {
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+                key={columnId}
+              >
+                <h2>{column.name}</h2>
+                <div style={{ margin: 8 }}>
+                  <Droppable droppableId={columnId} key={columnId}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                          style={{
+                            padding: 4,
+                            width: 250,
+                            minHeight: 500,
+                          }}
+                        >
+                          {column.items.map((item, index) => {
+                            return (
+                              <Draggable
+                                key={item.id}
+                                draggableId={item.id}
+                                index={index}
+                              >
+                                {(provided, snapshot) => {
+                                  return (
+                                    <div
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      style={{
+                                        userSelect: "none",
+                                        padding: 16,
+                                        margin: "0 0 8px 0",
+                                        minHeight: "50px",
+                                        backgroundColor: snapshot.isDragging
+                                          ? "#a333c8"
+                                          : "white",
+                                        color: "white",
+                                        ...provided.draggableProps.style,
+                                      }}
+                                    >
+                                      <div
+                                        className="ui fluid card"
+                                        key={index}
+                                        style={{ border: "1px solid #a333c8" }}
+                                      >
+                                        <div className="content center aligned">
+                                          <div className="header">
+                                            {item.title}
+                                          </div>
+                                          <div className="description">
+                                            Playing Time{" "}
+                                            <b>{item.duration} Min</b>
+                                          </div>
+                                          <div className="description">
+                                            Cost Per Recipient{" "}
+                                            <b>{item.costPerRecipient}$</b>
+                                          </div>
                                         </div>
                                       </div>
                                     </div>
-                                  </div>
-                                );
-                              }}
-                            </Draggable>
-                          );
-                        })}
-                        {provided.placeholder}
-                      </div>
-                    );
-                  }}
-                </Droppable>
+                                  );
+                                }}
+                              </Draggable>
+                            );
+                          })}
+                          {provided.placeholder}
+                        </div>
+                      );
+                    }}
+                  </Droppable>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </DragDropContext>
-    </div>
+            );
+          })}
+        </DragDropContext>
+      </div>
+      <div className="ui grid">
+        <div className="four wide column">
+          <Link to="/add-recipients" className="ui secondary basic button">
+            Back
+          </Link>
+        </div>
+        <div className="eight wide column">
+          <div className="ui divider">
+            <div style={{textAlign:"center"}}>Total cost of 5000$ for 3 recipient</div>
+          </div>
+        </div>
+        <div className="four wide column">
+          <button
+            onClick={onClick}
+            className="ui right floated primary basic button"
+            type="button"
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
